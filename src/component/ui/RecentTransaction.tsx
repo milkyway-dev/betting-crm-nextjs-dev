9d49ab64774f3793aa1139ec47d18a31837debe4
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-08-08 05:11:15 -0700

    Dashboard ui

--- src/component/ui/RecentTransaction.tsx
+++ src/component/ui/RecentTransaction.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RecentTransaction = () => {
   return (
     <div className='border-[1px] border-[#282828] text-white p-3 rounded-3xl'>
-      Recent Transaction
+       <div className='text-white px-6 tracking-wide py-2 bg-light_black rounded-3xl inline-block'>Recent Transactions</div>
     </div>
   )
 }
