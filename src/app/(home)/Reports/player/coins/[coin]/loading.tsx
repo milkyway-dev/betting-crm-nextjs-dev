128d5a845c67d93f2b4ad4f85a4207432133eee8
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-08-27 22:03:33 -0700

    Loader

--- src/app/(home)/Reports/player/coins/[coin]/loading.tsx
+++ src/app/(home)/Reports/player/coins/[coin]/loading.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function loading() {
   return (
-    <div className="fixed z-[9999] bg-black bg-opacity-20 top-0 left-0 w-full h-screen">
-    <div className="w-full h-screen relative  flex items-center justify-center">
-        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-600"></div>
-    </div>
-  </div>
+      <div className="fixed z-[9999] bg-black bg-opacity-20 top-0 left-0 w-full h-screen">
+        <div className="w-full h-screen relative  flex items-center justify-center">
+          <svg className="loader" viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+          </svg>
+        </div>
+      </div>
     )
 };
--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -4,7 +4,9 @@ export default function loading() {
   return (
       <div className="fixed z-[9999] bg-black bg-opacity-20 top-0 left-0 w-full h-screen">
         <div className="w-full h-screen relative  flex items-center justify-center">
-            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-yellow-600"></div>
+          <svg className="loader" viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+          </svg>
         </div>
       </div>
     )
