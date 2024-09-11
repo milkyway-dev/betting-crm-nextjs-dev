c910e0f2f8c81ab18ef652644f1264611edde747
rohit <rohit@underpinservices.com>
2024-09-11 16:57:21 +0530

    fix: config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,5 @@
 const _config = {
-  // server: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
-  server: "http://localhost:5000"
+  server: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
 };
 
 export const config = Object.freeze(_config);
