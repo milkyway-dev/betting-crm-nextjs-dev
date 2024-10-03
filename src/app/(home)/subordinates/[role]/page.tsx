ccf1b80f2cf568a58eddcb41cb0576fe2b52f33e
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-10-03 13:18:47 +0530

    Sifted Pagination on Client Side.

--- src/app/(home)/subordinates/[role]/page.tsx
+++ src/app/(home)/subordinates/[role]/page.tsx
@@ -1,15 +1,41 @@
-import LastItemDetector from "@/component/ui/LastItemDetector";
+"use client"
+import { useEffect, useRef, useState } from "react";
 import Table from "@/component/ui/Table";
 import { getSubordinates } from "@/utils/action";
 
-const Page = async ({ params, searchParams }: any) => {
-  const data = await getSubordinates(
-    params?.role,
-    searchParams?.search,
-    searchParams?.date,
-    searchParams?.page,
-    searchParams?.limit,
-  );
+const Page = ({ params, searchParams }: any) => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [pageCount, setPageCount] = useState<number>(1)
+  const lastElementRef = useRef(null);
+  console.log(data)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true); // Start loading
+        const result = await getSubordinates(
+          params?.role,
+          searchParams?.search,
+          searchParams?.date,
+          pageCount,
+          10,
+        );
+        console.log(result?.data,"data")
+        if ((searchParams?.search?.length > 0) || (searchParams?.date)) {
+          setData([...result?.data]);
+        } else {
+          const newData = result?.data?.filter((item: any) => !data.some((stateItem) => stateItem?._id === item?._id));
+          setData([...data,...newData]);
+        }
+      } catch (error) {
+        console.error("Error fetching subordinates:", error);
+      } finally {
+        setLoading(false); // End loading
+      }
+    };
+
+    fetchData();
+  }, [params?.role, searchParams?.search, searchParams?.date, pageCount]);
 
   const fieldsHeadings = [
     "Username",
@@ -29,10 +55,42 @@ const Page = async ({ params, searchParams }: any) => {
     "actions",
   ];
 
+
+  // Use IntersectionObserver to detect when the last element is in view
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0]?.isIntersecting && data?.length >= 10) {
+          setPageCount((prevPageCount) => prevPageCount + 1);
+        } ` `
+      },
+      {
+        threshold: 1
+      }
+    );
+
+    if (lastElementRef.current) {
+      observer.observe(lastElementRef.current);
+    }
+
+    return () => {
+      if (lastElementRef.current) {
+        observer.unobserve(lastElementRef.current);
+      }
+    };
+  }, [data]);
+
+
   return (
     <>
-      <Table fieldsHeadings={fieldsHeadings} searchDate={searchParams?.date} searchquery={searchParams?.search} fieldData={fieldsData} data={data?.data} />
-      <LastItemDetector searchDate={searchParams?.date} searchquery={searchParams?.search} data={data.data} />
+      <Table
+        fieldsHeadings={fieldsHeadings}
+        searchDate={searchParams?.date}
+        searchquery={searchParams?.search}
+        fieldData={fieldsData}
+        data={data}
+      />
+      <div ref={lastElementRef} style={{ height: '4px', width: '100%' }} />
     </>
   );
 };
--- src/component/ui/Modal.tsx
+++ src/component/ui/Modal.tsx
@@ -19,7 +19,7 @@ import { UpdateCredit } from "@/redux/ReduxSlice";
 import { useDispatch } from "react-redux";
 import Select from "react-select";
 import Image from "next/image";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const Modal: React.FC<ModalProps> = ({
   betId,
@@ -27,8 +27,6 @@ const Modal: React.FC<ModalProps> = ({
   onClose,
   Type,
   data,
-  Tabs = [],
-  Page,
   betData
 
 }) => {
@@ -37,7 +35,6 @@ const Modal: React.FC<ModalProps> = ({
   const [betStatus, setBetStatus] = useState<string>("lost");
   const dispatch = useDispatch();
   const [customStatus, setCustomStatus] = useState<string>('');
-  const router = useRouter()
   const [bannerPreview, setBannerPreview] = useState<any>();
   const [categories, setCategories] = useState<
     { value: string; label: string }[]
@@ -49,13 +46,16 @@ const Modal: React.FC<ModalProps> = ({
     banner: null,
     title: "",
   });
+
+
   //Edit
   const [formData, setFormData] = useState<EditFormData>({
     password: "",
     status: data?.status,
   });
-  // console.log(betData, "betdata");
-  // console.log(betId, "betID");
+
+
+
   const [betDetailStatus, setBetDetailStatus] = useState<any>();
   const [parentbetData, setParentBetData] = useState({
     betType: betData?.betType,
@@ -299,7 +299,7 @@ const Modal: React.FC<ModalProps> = ({
   };
   //Edit
   //Recharge
-  const [transaction, setTransaction] = useState("0");
+  const [transaction, setTransaction] = useState("");
   const handleRecharge = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const type = Type?.toLowerCase();
@@ -465,8 +465,14 @@ const Modal: React.FC<ModalProps> = ({
     });
   };
 
-
-
+  const handelClose = () => {
+    onClose();
+    setFormData({
+      password: "",
+      status: data?.status,
+    })
+    setTransaction("")
+  }
 
   switch (Type) {
     case "Delete":
@@ -474,7 +480,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed inset-0 flex items-center justify-center z-[100]"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -493,7 +499,7 @@ const Modal: React.FC<ModalProps> = ({
                   </button>
                   <button
                     className="px-6 py-1.5 rounded-3xl bg-gray-300 text-black hover:bg-gray-400 transition duration-200 ease-in-out"
-                    onClick={onClose}
+                    onClick={handelClose}
                   >
                     No
                   </button>
@@ -510,7 +516,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed inset-0 flex items-center justify-center z-[100]"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -527,7 +533,7 @@ const Modal: React.FC<ModalProps> = ({
                         type="password"
                         name="password"
                         placeholder="Enter new password"
-                        value={formData.password}
+                        value={formData?.password}
                         onChange={(e) =>
                           setFormData({ ...formData, password: e.target.value })
                         }
@@ -542,7 +548,7 @@ const Modal: React.FC<ModalProps> = ({
                     <div className="bg-[#1A1A1A] flex pl-4 items-center  dark:bg-onDark dark:border-opacity-30 mb-5 border-opacity-60 border-dark_black rounded-lg border-[1px] relative">
                       <select
                         name="status"
-                        value={formData.status}
+                        value={formData?.status}
                         onChange={(e) =>
                           setFormData({ ...formData, status: e.target.value })
                         }
@@ -572,7 +578,7 @@ const Modal: React.FC<ModalProps> = ({
                       SAVE
                     </button>
                     <button
-                      onClick={onClose}
+                      onClick={handelClose}
                       className="text-white w-[90%] bg-[#c6c6c63b] uppercase border-[2px] dark:text-black border-black dark:border-opacity-50 text-sm text-center py-3 rounded-xl shadow-sm"
                     >
                       CANCLE
@@ -592,7 +598,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed z-[100] inset-0 flex items-center justify-center"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -616,7 +622,7 @@ const Modal: React.FC<ModalProps> = ({
                   </div>
                   <div className="flex space-x-4 justify-center pt-4">
                     <button
-                      onClick={onClose}
+                      onClick={handelClose}
                       className="text-white w-[90%] bg-[#c6c6c63b] uppercase border-[2px]  dark:text-black border-black dark:border-opacity-40 text-sm text-center py-3 rounded-xl shadow-sm"
                     >
                       CANCLE
@@ -641,7 +647,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed z-[100] inset-0 flex items-center justify-center"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -675,7 +681,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed z-[100] inset-0 flex items-center justify-center"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -786,7 +792,7 @@ const Modal: React.FC<ModalProps> = ({
         <>
           <div
             className="fixed inset-0 flex items-center justify-center z-[100]"
-            onClick={onClose}
+            onClick={handelClose}
           >
             <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
               <div
@@ -902,7 +908,7 @@ const Modal: React.FC<ModalProps> = ({
                       Upload
                     </button>
                     <button
-                      onClick={onClose}
+                      onClick={handelClose}
                       className="text-white w-[90%] bg-[#c6c6c63b] uppercase border-[2px] dark:text-black border-black dark:border-opacity-50 text-sm text-center py-3 rounded-xl shadow-sm"
                     >
                       cancel
